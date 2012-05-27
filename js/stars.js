3b54981d93d1f2a8fe4c225a54d25eba51449344
FabienSalles <blacked19@gmail.com>
2012-05-27 20:36:54 +0200

    Utilisation de requirejs

---
+++ js/stars.js
@@ -0,0 +1,46 @@
+	/**
+	 * Class Star
+	 */
+	var Stars = new Class({
+		
+		/** Constructor */
+		initialize	: function( elem ){
+			
+			// Must be a CSS selector
+			this.elem = $$( elem );
+			
+			// Stars generation
+			this.elem.each( this.generateStars.bind( this ) );
+		},
+		
+		generateStars : function( el ){
+			
+			var note = el.getProperty('data-note'),				// Note of the skill
+				integer = note == Math.floor(note),				// Boolean
+				stars = new Array,								// Array of stars
+				i = 0;											// iterator
+			
+			if(!integer) note = Math.floor(note);				// If note isn't an integer note becomes an integer
+			
+			// Full stars
+			for(;i<note;i++)
+				stars.push(this.createStar(1));
+			
+			// Half full star
+			if(!integer){
+				stars.push(this.createStar(2));
+				i++;
+			}
+			
+			// Empty stars
+			for(;i<5;i++)
+				stars.push(this.createStar(3));
+			
+			el.adopt(stars);
+		},
+		
+		createStar		: function( num ){
+			
+			return new Element('span.star'+num);
+		}
+	});
